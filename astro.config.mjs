dd7d27259d6d948d4b6e54b18521290d5e797088
CristianRoa33 <ccroa33@gmail.com>
2026-01-30 17:42:39 -0500

    firts_commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({ 
+  output: 'static',
   vite: {
     resolve: {
       alias: {
         '@assets': '/src/assets',   // aquí defines el alias
         '@components': '/src/components', // opcional, para tus componentes
         '@layouts': '/src/layouts'  // opcional, para layouts
+        
       },
     },
   },
