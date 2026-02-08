8cd5540779b6de0be341ddff9906c854c71c3d4d
CristianRoa33 <ccroa33@gmail.com>
2026-02-07 19:10:04 -0500

    readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({ 
   output: 'static',
+  site: 'https://cristianroa.github.io',
   base: '/Proyecto-Billeteras/',
   vite: {
     resolve: {
